66b87a2ced34b687ab22003bbb31a66ad3278651
YashTote <yashtote9999@gmail.com>
2023-09-07 17:31:10 +0530

    Created DB Schema

--- emailCampaign/endpoint/models.py
+++ emailCampaign/endpoint/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Campaign(models.Model):
+    title = models.CharField(max_length=50)
+    subject = models.CharField(max_length=255)
+    preview_text = models.TextField()
+    article_url = models.URLField()
+    html_content = models.TextField()
+    plain_text_content = models.TextField()
+    published_date = models.DateTimeField()
+    
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+    
+class CategoryRelationship(models.Model):
+    subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.subscriber.first_name} - {self.category.name} - {self.campaign.title}"
